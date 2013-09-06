1dc77af85b0b540faea75a6f7d266776cb913e83
Ronie Uliana <ronie.uliana@gmail.com>
2013-09-05 23:17:35 -0300

    Adding a minor niceties.
    A little documentation here and there, plus a sugar method.

--- lib/deadly_serious/engine/base_process.rb
+++ lib/deadly_serious/engine/base_process.rb
@@ -8,16 +8,27 @@ module DeadlySerious
         reader.each { |packet| super(packet.chomp) }
       end
 
+      # Alias to #send
+      def emit(packet = nil)
+        send(packet)
+      end
+
+      # Send a packet to the next process
       def send(packet = nil)
         send_buffered(packet)
         flush_buffer
       end
 
+      # Send a packet to the next process,
+      # however, accumulate some of them
+      # before send to gain a little
+      # efficency.
       def send_buffered(packet = nil)
         @writer << packet if packet
         @writer << "\n"
       end
 
+      # Send all not yet sent packets.
       def flush_buffer
         @writer.flush
       end
