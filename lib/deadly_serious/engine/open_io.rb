c6cc62d94a1908bff9554220eb8c479fdd23d66f
Ronie Uliana <ronie.uliana@vagas.com.br>
2013-10-01 17:11:19 -0300

    Better error handling
    When something wrong happens in the process, the act of "close the pipe"
    could throw another error, hiding the first one.
    
    We are now eating the error when closing the pipe (we need a log there).

--- lib/deadly_serious/engine/open_io.rb
+++ lib/deadly_serious/engine/open_io.rb
@@ -9,14 +9,25 @@ module DeadlySerious
         super(*args, readers: opened_readers, writers: opened_writers)
       ensure
         if opened_writers
-          opened_writers.each { |writer| writer.close unless writer.closed? }
+          opened_writers.each { |writer| close_io(writer) }
         end
         if opened_readers
-          opened_readers.each { |reader| reader.close unless reader.closed? }
+          opened_readers.each { |reader| close_io(reader) }
         end
       end
 
       private
+
+      def close_io(io)
+        return unless io
+        return if io.closed?
+        io.close
+      rescue => e
+        # Intentionally eat the error
+        # because it's being used inside
+        # an "ensure" block
+        puts e.inspect
+      end
 
       def read_pipe(pipe_name)
         Channel.new(pipe_name).open_reader
