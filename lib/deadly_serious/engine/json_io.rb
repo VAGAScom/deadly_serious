d876d1bc9df9bb6de1b2838223cfa1bba1561cbc
Ronie Uliana <ronie.uliana@vagas.com.br>
2014-02-25 11:32:28 -0300

    Expanding JsonIo to Enumerable

--- lib/deadly_serious/engine/json_io.rb
+++ lib/deadly_serious/engine/json_io.rb
@@ -3,22 +3,14 @@ require 'json'
 module DeadlySerious
   module Engine
     class JsonIo
+      include Enumerable
+
       def initialize(io)
         @io = io
       end
 
       def each
         @io.each { |line| yield JSON.parse(line) }
-      end
-
-      def each_cons(qty)
-        @io.each_cons(qty) do |args|
-          yield *(args.map { |line| JSON.parse(line) })
-        end
-      end
-
-      def each_with_object(object)
-        @io.each_with_object(object) { |line, object| yield JSON.parse(line), object }
       end
 
       def <<(value)
