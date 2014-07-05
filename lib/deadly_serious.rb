02cb6308149a40ea3a1e717f71812aaf5f428a0e
Ronie Uliana <ronie.uliana@vagas.com.br>
2014-07-05 11:28:20 -0300

    Preparing tests

--- lib/deadly_serious.rb
+++ lib/deadly_serious.rb
@@ -3,9 +3,12 @@ require 'deadly_serious/engine/commands'
 require 'deadly_serious/engine/spawner'
 
 # Loading all predefined processes
-Dir[File.dirname(__FILE__) + '/deadly_serious/processes/*.rb'].each do |file|
+Dir[File.join(File.dirname(__FILE__), 'deadly_serious', 'processes', '*.rb'].each do |file|
   require File.dirname(file) + '/' + File.basename(file, File.extname(file))
 end
 
 module DeadlySerious
+  def root
+    @root ||= File.expand_path(File.dirname(__FILE__))
+  end
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,1 @@
+require 'deadly_serious'
