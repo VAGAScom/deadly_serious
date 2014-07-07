d24baaaecc34633fe8e90e7e6204f1c49aa9ebe3
Ronie Uliana <ronie.uliana@vagas.com.br>
2014-07-07 06:52:53 -0300

    Fix bad require :(

--- lib/deadly_serious.rb
+++ lib/deadly_serious.rb
@@ -5,6 +5,10 @@ require 'deadly_serious/engine/base_process'
 require 'deadly_serious/engine/push_pop'
 require 'deadly_serious/engine/push_pop_send' # Compatibility with 0.2.0
 
+Dir[File.dirname(__FILE__) + '/deadly_serious/engine/*.rb'].each do |file|
+  require File.dirname(file) + '/' + File.basename(file, File.extname(file))
+end
+
 # Loading all predefined processes
 Dir[File.dirname(__FILE__) + '/deadly_serious/processes/*.rb'].each do |file|
   require File.dirname(file) + '/' + File.basename(file, File.extname(file))
